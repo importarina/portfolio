3bc6ee025677093a60141f1e754a8f59d2f597f4
arinamomajjed <arinamomajjed@gmail.com>
2025-05-24 21:15:57 -0400

    heroku backend url for contact form on frontend

--- components/features/contact/index.tsx
+++ components/features/contact/index.tsx
@@ -126,7 +126,7 @@ export function Contact() {
       }
 
       console.log("Sending request to backend...")
-      const response = await fetch("http://127.0.0.1:5000/api/contact", {
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/api/contact`, {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
